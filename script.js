4951b624fc24b7775a6f615313cb21789154663a
CasteAwayy <140751958+CasteAwayy@users.noreply.github.com>
2024-11-27 01:18:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-//your JS code here. If required.
+const buttonModal = document.querySelector('#openModal');
+const overlay = document.querySelector('.overlay');
+const closeModal = document.querySelector('.close-modal');
+
+const modal = document.querySelector('.modal')
+buttonModal.addEventListener('click',(e)=>{
+    e.preventDefault();
+    modal.classList.add('active');
+    overlay.style.display = 'block';
+})
+
+closeModal.addEventListener('click',(e)=>{
+    e.preventDefault();
+    modal.classList.remove('active');
+    overlay.style.display = 'none';
+})
+
+overlay.addEventListener('click',()=>{
+    // console.log('click')
+    modal.classList.remove('active');
+    overlay.style.display = 'none';
+})
